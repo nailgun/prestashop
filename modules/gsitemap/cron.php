fcdc64efe4dc9b356e367ea0c3015cf526a17850
Dmitry Bashkatov <dbashkatov@gmail.com>
2011-08-26 22:06:10 +0400

    fix gsitemap cronjob

--- modules/gsitemap/cron.php
+++ modules/gsitemap/cron.php
@@ -26,6 +26,7 @@
 */
 
 include(dirname(__FILE__).'/../../config/config.inc.php');
+require_once(dirname(__FILE__).'/../../init.php');
 include(dirname(__FILE__).'/gsitemap.php');
 
 if (isset($_GET['secure_key']))
